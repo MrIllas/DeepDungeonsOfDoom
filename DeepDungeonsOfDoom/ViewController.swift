ceb7235c75c97c13c5495ad0632989e7ed905c62
Robert Recorda <alu2015324@a22o26.local>
2018-11-28 19:32:11 +0100

    background

--- DeepDungeonsOfDoom/ViewController.swift
+++ DeepDungeonsOfDoom/ViewController.swift
@@ -14,7 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBOutlet weak var btnChar1: UIButton!
+    @IBOutlet weak var btnChar2: UIButton!
+    @IBOutlet weak var btnChar3: UIButton!
+    
+    @IBAction func selChar(_ sender: AnyObject) {
+        switch sender.tag{
+            case 1:
+                print("1")
+                break
+            case 2:
+                print("2")
+                break
+            case 3:
+                print("3")
+                break
+            default:
+                print("Woops!")
+        }
+    }
 
 }
-
